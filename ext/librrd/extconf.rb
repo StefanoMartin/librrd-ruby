4aea62854eb413113fecdfcb4b52ea878c7d8639
StefanoMartin <stefano@seluxit.com>
2016-07-29 15:27:54 +0200

    BAH!

--- ext/librrd/extconf.rb
+++ ext/librrd/extconf.rb
@@ -16,15 +16,15 @@ dir_config("rrd")
 # Try to detect the librrd version.
 # Please adjust if needed and open an issue on github
 # or submit a pull request. Thanks!
-if have_library("rrd", "rrd_flushcached")
-  src_prefix = '1.4'
-elsif have_type("rrd_info_t", "rrd.h") and have_library("rrd", "rrd_create")
-  src_prefix = '1.3'
-elsif have_library("rrd", "rrd_create")
-  src_prefix = '1.2'
-else
-  puts "Unsupported librrd version, abort."
-  exit 1
-end
+# if have_library("rrd", "rrd_flushcached")
+#   src_prefix = '1.4'
+# elsif have_type("rrd_info_t", "rrd.h") and have_library("rrd", "rrd_create")
+#   src_prefix = '1.3'
+# elsif have_library("rrd", "rrd_create")
+#   src_prefix = '1.2'
+# else
+#   puts "Unsupported librrd version, abort."
+#   exit 1
+# end
 
-create_makefile("RRD", '$(srcdir)/' + src_prefix)
+create_makefile("RRD", '$(srcdir)/1.4')
